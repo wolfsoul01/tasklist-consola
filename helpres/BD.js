318acaef6726683693512131aac1732e9504faa3
wolfsoul <alejandrof011021@gmail.com>
2023-05-03 12:14:52 -0400

    Se agrego los comentarios en el archivo BD

--- helpres/BD.js
+++ helpres/BD.js
@@ -1,11 +1,18 @@
 import fs from "fs";
 
+/* Aqui van las funciones que se enargan de controlar la BD */
+
+
+// ruta de la DB 
 const archivo = "./db/data.json";
 
+// Para guardar 
 export const guardarBD = (data) => {
   fs.writeFileSync(archivo, JSON.stringify(data));
 };
 
+
+//Para cargar las tareas 
 export const leerBaseDeatos = () => {
   if (!fs.existsSync(archivo)) {
     return null;
